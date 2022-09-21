4021f51262971cd0d62f1a4eb10dfb51355883dc
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2022-09-21 12:42:50 +0300

    Add files via upload

---
+++ AdventureGame/Archer.java
@@ -0,0 +1,8 @@
+package AdventureGame;
+
+public class Archer extends GameCharacter{
+    public Archer() {
+        super("Archer", 2, 7, 18, 20);
+
+    }
+}
---
+++ AdventureGame/Knight.java
@@ -0,0 +1,7 @@
+package AdventureGame;
+
+public class Knight extends GameCharacter{
+    public Knight() {
+        super("Knight", 3, 8, 24, 5);
+    }
+}
